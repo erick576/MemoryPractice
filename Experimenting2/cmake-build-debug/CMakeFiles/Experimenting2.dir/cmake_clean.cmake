2b00a38dec7d64bcae34e3acc6b3375a0986a3b2
Eric Kwok <46385457+erick576@users.noreply.github.com>
2020-05-05 22:21:58 -0400

    Add files via upload

---
+++ Experimenting2/cmake-build-debug/CMakeFiles/Experimenting2.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Experimenting2.dir/src/ListNode.cpp.obj"
+  "CMakeFiles/Experimenting2.dir/test/linkedTests.cpp.obj"
+  "CMakeFiles/Experimenting2.dir/test/tests-main.cpp.obj"
+  "Experimenting2.exe"
+  "Experimenting2.exe.manifest"
+  "Experimenting2.pdb"
+  "libExperimenting2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Experimenting2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
